11c7c56e2216499a7bb43f5bc5e30ad06252fbca
RevaKaushal <reva.kaushal93@gmail.com>
2022-11-05 23:32:43 +0530

    abc commit

---
+++ tut21_friend.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+class complex; //forward declaration --> of class for class using it beforehand
+
+class calculator{
+   public:
+    int sumreal(complex,complex);
+    int sumcomplex(complex,complex);
+};
+
+class complex{  //friend-->allowance for usage of PRIVATE MEMBERS of a class to another NON MEMBER function or class
+  int a,b;  
+  // Individually declaring functions as friends
+    // friend int Calculator ::sumreal(Complex, Complex);
+    // friend int Calculator ::sumComplex(Complex, Complex);
+
+ // Aliter: Declaring the entire calculator class as friend
+  friend class calculator;
+  public:
+    void setnumber(int x,int y){
+        a=x;
+        b=y;
+    }
+    void printnumber(){
+        cout<<"the complex number is: "<<a<<" + "<<b<<"i"<<endl;
+    }
+ };
+ 
+int calculator::sumreal(complex o1,complex o2){
+   return(o1.a+o2.a);
+}
+int calculator::sumcomplex(complex o1, complex o2){
+    return(o1.b+o2.b);
+}
+
+int main(){
+    complex c1,c2;
+    c1.setnumber(1,4);
+    c1.printnumber();
+
+    c2.setnumber(4,5);
+    c2.printnumber();
+    
+    calculator calc;
+    int r= calc.sumreal(c1,c2);
+    cout<<"the real sum is "<<r<<endl;
+    int c= calc.sumcomplex(c1,c2); 
+    cout<<"the complex sum is "<<c<<endl;
+    return 0;
+}
